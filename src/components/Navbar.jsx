3993230f1fa49ae034a9fae20d79ec405dacbc5a
GautamPatil <gautampatil4545@outlook.com>
2024-04-05 15:23:32 +0530

    Added K8 Deployment.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,24 @@
 // Navbar.js
 
-import React from 'react';
+import React from "react";
 import styles from "./styles/Navbar.module.css";
 
 export default function Navbar() {
   return (
-    <div className={styles.navbarContainer}>  
+    <div className={styles.navbarContainer}>
       <div className={styles.brand}>
         <div className={styles.logo}>
-          <img
-            src="/logo.png"
-            alt="rezumelogo"
-            style={{
-              display: "inline",
-              width: "50px",
-              height: "50px",
-            }}
-          />
+          <a href="/">
+            <img
+              src="/logo.png"
+              alt="rezumelogo"
+              style={{
+                display: "inline",
+                width: "50px",
+                height: "50px",
+              }}
+            />
+          </a>
         </div>
         <div className={styles.name}>
           <a href="/">REZUME</a>
@@ -24,14 +26,10 @@ export default function Navbar() {
       </div>
 
       <div className={styles.right}>
-        <div className={styles.howItWorks}>
-          How it works
-        </div>
+        <div className={styles.howItWorks}>How it works</div>
 
         <div className={styles.auth}>
-          <div className={styles.login}>
-            Start Rezume
-          </div>
+          <div className={styles.login}>Start Rezume</div>
         </div>
       </div>
     </div>
